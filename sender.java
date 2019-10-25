74d40f2f76f74fae94f94163f5a9ad8799726a2c
alibagher <a6bagher@uwaterloo.ca>
2019-10-24 20:04:05 -0400

    first commit

---
+++ sender.java
@@ -0,0 +1,26 @@
+// sender code
+// Ali Bagheri Nov. 2019
+
+//imports
+import java.net.*;
+import java.io.*;
+
+import  java.lang.*;
+import java.util.concurrent.TimeUnit;
+
+// scanner
+import java.util.Scanner;
+
+public class sender {
+
+    public static void main(String args[]) throws IOException {
+        int hostAddr = Integer.valueOf(args[0]).intValue();
+        int emulatorPort = Integer.valueOf(args[1]).intValue();
+        int recievePort = Integer.valueOf(args[2]).intValue();
+        String fName = args[3];
+
+        
+
+    }
+
+}
